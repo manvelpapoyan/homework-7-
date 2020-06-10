a6c0e2f09b57247c97135c08ea535a8ccb3a34d9
manvelpapoyan <58630681+manvelpapoyan@users.noreply.github.com>
2020-06-10 23:51:54 +0400

    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -1,87 +1,87 @@
 class Author {
-    constructor() {
-        this._name
-        this._email 
-        this._gender 
-    }
+  constructor(name, email, gender) {
+    this.name = name;
+    this.email = email;
+    this.gender = gender;
+  }
 
-    set name(a) {
-        this._name = a;
-    }
+  set name(a) {
+    this._name = a;
+  }
 
-    get name() {
-        return this._name
-    }
+  get name() {
+    return this._name;
+  }
 
-    set gender(b) {
-        this._gender = b;
-    }
+  set gender(b) {
+    this._gender = b;
+  }
 
-    get gender() {
-        return this._gender
-    }
+  get gender() {
+    return this._gender;
+  }
 
-    set email(c) {
-        this._email = c;
-    }
+  set email(c) {
+    this._email = c;
+  }
 
-    get email() {
-        return this._email
-    }
+  get email() {
+    return this._email;
+  }
 
-    toString() {
-        return `${this.name}\n ${this.email}\n ${this.gender}`
-    }
+  toString() {
+    return `${this.name}\n ${this.email}\n ${this.gender}`;
+  }
 }
 
-
 class Book {
+  constructor(title, author, price, quantity) {
+    this.title = title;
+    this.author = author;
+    this.price = price;
+    this.quantity = quantity;
+  }
 
-    constructor() {
-        this._title 
-        this._author = new Author()
-        this._price 
-        this._quantity
+  set title(a) {
+   
+      this._title = a;
+    
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set author(b) {
+    if (b instanceof Author) {
+      this._author = b;
     }
-    set title(a) {
-        this._title = a
-    }
+  }
 
-    get title() {
-        return this._title
-    }
+  get author() {
+    return this._author;
+  }
 
-    set author(b) {
-        this._author = a
-    }
+  set price(c) {
+    this._price = c;
+  }
 
-    get author() {
-        return this._author
-    }
+  get price() {
+    return this._price;
+  }
 
-    set price(c) {
-        this._price = c
-    }
+  set quantity(d) {
+    this._quantity = d;
+  }
 
-    get price() {
-        return this._price
-    }
+  get quantity() {
+    return this._quantity;
+  }
 
-    set quantity(d) {
-        this._quantity = d
-    }
-
-    get quantity() {
-        return this._quantity
-    }
-
-    getProfit(){
-
-        return this.quantity*this.price
-
-    }
-    toString(){
-        return `${this.title}\n ${this.author.name}\n ${this.price}`
-    }
-
+  getProfit() {
+    return this.quantity * this.price;
+  }
+  toString() {
+    return `${this.title}\n ${this.author.name}\n ${this.price}`;
+  }
 }
